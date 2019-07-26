ed920d1fdbf5fa8df9ebda98c3a8026dc4f58099
paocastrob <paocastrob@gmail.com>
2019-07-26 19:43:56 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,46 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	componentDidMount() {
+		{
+			/*this.pauseBtn.style.display = "none";*/
+		}
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(resp => resp.json())
+			.then(songs => this.setState({ songs }));
+	}
+
+	play() {
+		console.log("clicked");
+	}
+
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+				<h1>Paolas App!</h1>
+				<div className="song">Song here</div>
+				<div className="song">Song here</div>
+				<div className="song">Song here</div>
+				<div className="song">Song here</div>
+				<div className="song">Song here</div>
+
+				<div className="control">
+					<div
+						href="#"
+						ref={elm => (this.playBtn = elm)}
+						onClick={() => this.play()}>
+						<i className="fas fa-play faicon" />
+					</div>
+					<div>
+						Pause <i className="fas fa-pause faicon" />
+					</div>
+					<div>
+						Next <i className="fas fa-forward faicon" />
+					</div>
+					<div>
+						Reverse <i className="fas fa-backward faicon" />
+					</div>
+				</div>
 			</div>
 		);
 	}
